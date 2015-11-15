e4c5a1254c139ba83aaa156784af87821ced65cd
Julio Foulquie <jfoulquie@gmail.com>
2015-11-15 14:05:42 -0300

    Create propuestas migration.

---
+++ database/migrations/2015_11_15_112141_propuestas.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Propuestas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('propuestas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre')->unique();
+            $table->string('contenidos');
+            $table->string('thumbnail');
+            $table->string('archivo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('propuestas');
+    }
+}
