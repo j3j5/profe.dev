20d3056f28f081301815213c7f1791b74303741a
Julio Foulquie <jfoulquie@gmail.com>
2016-02-08 17:09:58 -0300

    Add preguntas/respuestas to the admin panel.

--- config/vivifyideas/admin-panel-generator.php
+++ config/vivifyideas/admin-panel-generator.php
@@ -5,59 +5,95 @@ return [
 
     'authMiddleware' => 'auth',
 
-    'tables' => ['users', 'propuestas'],
+    'tables' => ['users', 'propuestas', 'preguntas', 'respuestas'],
 
     'rowsPerPage' => 15,
 
     'additionalLinks' => [],
 
-    'columns' => [
-        'users' => [
-            'id', 'name', 'email', 'created_at'
-        ],
-        'propuestas' => [
-            'nombre', 'contenidos', 'thumbnail', 'archivo', 'updated_at',
-        ],
+    'columns'   => [
+        'users'         => ['id', 'name', 'email', 'created_at'],
+        'propuestas'    => ['nombre', 'contenidos', 'thumbnail', 'archivo', 'updated_at',],
+        'preguntas'     => ['pregunta', 'practica', 'updated_at'],
+        'respuestas'     => ['respuesta', 'updated_at'],
     ],
 
     'filters' => [
-        'users' =>
+        'users'     =>
             [
                 'email' => [
-                    'label' => 'Email',
-                    'type' => 'text',
-                    'compare' => 'LIKE',
+                    'label'     => 'Email',
+                    'type'      => 'text',
+                    'compare'   => 'LIKE',
                 ],
                 'name' => [
-                    'label' => 'Nombre',
-                    'type' => 'text',
-                    'compare' => 'LIKE',
+                    'label'     => 'Nombre',
+                    'type'      => 'text',
+                    'compare'   => 'LIKE',
+                ],
+            ],
+        'preguntas' =>
+            [
+                'practica' => [
+                    'label' => '¿Es de práctica?',
+                    'type'  => 'checkbox',
                 ],
             ],
     ],
     'forms' => [
-        'users' => [
+        'users'         => [
             'name',
             'email',
         ],
-        'propuestas' => [
+        'propuestas'    => [
             'nombre',
             'contenidos',
             'thumbnail',
             'archivo',
         ],
+        'preguntas'     => [
+            'pregunta',
+            'practica' => [
+                'label' => '¿Es de prácticas?',
+                'type' => 'checkbox',
+            ],
+            'hasMany' => [
+                'respuestas' => [
+                    'label' => 'Respuesta',
+                    'column' => 'id',
+                    'foreignLabel' => 'respuesta'
+                ]
+            ],
+        ],
+        'respuestas'    => [
+            'respuesta',
+            'belongsTo' => [
+                'preguntas' => [
+                    'label' => 'Pregunta',
+                    'column' => 'pregunta_id',
+                    'foreignLabel' => 'pregunta',
+                ]
+            ],
+        ],
     ],
 
     'validationRules' => [
-        'users' => [
-            'name' => 'required|string',
+        'users'         => [
+            'name'  => 'required|string',
             'email' => 'required|email',
         ],
-        'propuestas' => [
-            'nombre' => 'required|string',
-            'contenidos' => 'required|string',
-            'thumbnail' => 'string',
-            'archivo' => 'string',
+        'propuestas'    => [
+            'nombre'        => 'required|string',
+            'contenidos'    => 'required|string',
+            'thumbnail'     => 'string',
+            'archivo'       => 'string',
+        ],
+        'preguntas'     => [
+            'pregunta'  => 'required|string',
+            'practica'  => 'boolean',
+        ],
+        'respuestas'    => [
+            'respuesta' => 'required|string',
         ],
     ]
 ];
