5cc61ee9adca568716a31e9ad03a8fbf837b9e11
Julio Foulquie <jfoulquie@gmail.com>
2016-03-20 13:34:22 +0000

    Change buttons style on admin index.

--- resources/views/vendor/vivifyideas/admin-panel-generator/main/index.blade.php
+++ resources/views/vendor/vivifyideas/admin-panel-generator/main/index.blade.php
@@ -19,7 +19,7 @@
                   {!! Form::{$options['type']}($name, @$filterValue[$name], ['class'=>'form-control']) !!}
                 </div>
               @endforeach
-              <button class="btn btn-default" type="submit">{{ packageTranslation('vivify.filter-verb') }}</button>
+              <button class="btn btn-info" type="submit">{{ packageTranslation('vivify.filter-verb') }}</button>
               <a href="/{{ packageConfig('prefix') }}/{{ $tableName }}" class="btn btn-warning">{{ packageTranslation('vivify.reset') }}</a>
             </form>
           </div>
@@ -51,7 +51,7 @@
                 @endforeach
                 <td class="text-right">
                     <div class="btn-group">
-                        <a class="btn btn-xs btn-default" href="/{{ packageConfig('prefix') }}/{{ $tableName }}/edit/{{ $row->id }}">
+                        <a class="btn btn-xs btn-info" href="/{{ packageConfig('prefix') }}/{{ $tableName }}/edit/{{ $row->id }}">
                             <i class="fa fa-4 fa-pencil-square-o"></i>
                         </a>
                         <a class="btn btn-xs btn-danger" onclick="return confirm('{{ packageTranslation('vivify.confirmation') }}')" href="/{{ packageConfig('prefix') }}/{{ $tableName }}/delete/{{ $row->id }}">
