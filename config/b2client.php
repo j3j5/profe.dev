f9eca9d124d183ddbe5a908cd5a9c73931f7f8bc
Julio Foulquie <jfoulquie@gmail.com>
2016-03-21 17:09:50 +0000

    Add bucket_name to the config file.

--- config/b2client.php
+++ config/b2client.php
@@ -4,4 +4,5 @@ return [
     "account_id"    => env("B2_ACCOUNT_ID", ""),
     "app_key"       => env("B2_APPLICATION_KEY", ""),
     "bucket_id"     => env("B2_BUCKET_ID", ""),
+    "bucket_name"   => env("B2_BUCKET_NAME", ""),
 ];
