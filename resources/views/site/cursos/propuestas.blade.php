54499e694731bbebc24d4b0dca52d43d688152be
Julio Foulquie <jfoulquie@gmail.com>
2016-03-24 01:28:59 +0000

    Add a row to propuestas grid (each 3).

--- resources/views/site/cursos/propuestas.blade.php
+++ resources/views/site/cursos/propuestas.blade.php
@@ -7,7 +7,12 @@
 
     @if(!$propuestas->isEmpty())
     <div class="container">
+        <?php $counter = 0; ?>
         @foreach($propuestas as $propuesta)
+        <?php $counter++; ?>
+        @if($counter%3 == 0)
+        <div class="row">
+        @endif
             <div class="col-md-4">
                 <a href="@if(app()->environment('production'))https://f001.backblaze.com/file/{{ config('b2client.bucket_name') }}@endif/uploads/{{ $propuesta->archivo }}" target="_blank">
                     <div class="">
@@ -22,14 +27,19 @@
                         <div class="propuesta-info text-center">
                             <h2 class="propuesta-nombre">{{ $propuesta->nombre }}</h2>
                             <p class="contenidos">
+                            @if(!empty($propuesta->contenidos))
                             @foreach($propuesta->contenidos as $contenido)
                                 <span class="contenido">#{{ trim($contenido) }}</span>
                             @endforeach
+                            @endif
                             </p>
                         </div>
                     </div>
                 </a>
             </div>
+        @if($counter%3 == 0)
+        </div>
+        @endif
         @endforeach
     </div>
     @else
