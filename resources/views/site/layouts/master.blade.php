eddbdb7dd51319001fa9f2cc3b3bc606342e226d
Julio Foulquie <jfoulquie@gmail.com>
2016-03-25 00:11:38 -0300

    Ädd favicon.

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <meta name="_token" content="{{ csrf_token() }}">
+        <link rel="icon" type="image/png" href="/favicon.ico" />
         <title>Profe Mariana</title>
                 <!-- css files -->
         {{ Asset::css() }}
