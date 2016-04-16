8a846d0fe384b94f4a31caef6c645f3681a882e6
Julio Foulquie <jfoulquie@gmail.com>
2016-04-16 02:43:23 +0000

    Add print style.

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="es">
+<html lang="es" moznomarginboxes mozdisallowselectionprint>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
