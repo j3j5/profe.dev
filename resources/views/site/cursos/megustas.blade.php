ed630adea256d0321c83a08dc80e56cf35e82a7c
Julio Foulquie <jfoulquie@gmail.com>
2016-04-22 13:24:26 -0300

    First version for like page.

--- resources/views/site/cursos/megustas.blade.php
+++ resources/views/site/cursos/megustas.blade.php
@@ -7,12 +7,16 @@
     @if(!$likes->isEmpty())
     <div class="container">
         <div id="links" class="text-center">
-            @foreach($likes as $like)
-            <div class="megusta-gallery">
-                <a href="@if(app()->environment('production'))https://f001.backblaze.com/file/{{ config('b2client.bucket_name') }}@endif/uploads/{{ $like->imagen }}" title="{{ $like->titulo }}" data-gallery>
-                    <img src="@if(app()->environment('production'))https://f001.backblaze.com/file/{{ config('b2client.bucket_name') }}@endif/uploads/{{ $like->imagen}}">
-                </a>
-            </div>
+            @foreach($titulos as $titulo)
+                <h2><span class="label label-default seccion">{{ $titulo->titulo }}</span></h2>
+                <?php $title_likes = $likes->where('titulo', $titulo->titulo ); ?>
+                @foreach($title_likes as $like)
+                <div class="megusta-gallery">
+                <a href="@if(app()->environment('production'))https://f001.backblaze.com/file/{{ config('b2client.bucket_name') }}@endif/uploads/{{ $like->imagen }}" title="{{ $like->autor }}" data-author="{{ $like->autor }}" data-gallery>
+                        <img src="@if(app()->environment('production'))https://f001.backblaze.com/file/{{ config('b2client.bucket_name') }}@endif/uploads/{{ $like->imagen}}" alt="">
+                    </a>
+                </div>
+                @endforeach
             @endforeach
         </div>
     </div>
