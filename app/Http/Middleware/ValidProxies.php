10250cb5f851d56f2f164c9a7606482f4e8c4cbc
Julio Foulquie <jfoulquie@gmail.com>
2016-06-21 22:52:12 +0000

    Add valid proxies so the requests are set as secure.

--- app/Http/Middleware/ValidProxies.php
+++ app/Http/Middleware/ValidProxies.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class EnforceSecureConnection
+class ValidProxies
 {
     /**
      * Handle an incoming request.
@@ -15,10 +15,9 @@ class EnforceSecureConnection
      */
     public function handle($request, Closure $next)
     {
-        if (!request()->secure() && !app()->environment('local')) {
-            return redirect()->secure($request->getRequestUri());
-        }
-
+        // Proxies
+        $request->setTrustedProxies([ $request->getClientIp() ]);
+        
         return $next($request);
     }
 }
