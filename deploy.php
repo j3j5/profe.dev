7a205bdfa6c6481b339d09c565549d2c7df2dbe2
Julio Foulquie <jfoulquie@gmail.com>
2016-11-05 21:17:06 -0300

    Start using Pablo's deployer script.

--- deploy.php
+++ deploy.php
@@ -1,14 +1,31 @@
 <?php
-/*
- * This file has been generated automatically.
- * Please change the configuration for correct use deploy.
- */
 
-require 'recipe/laravel.php';
+require_once 'recipe/common.php';
+require_once __DIR__ . '/deploy/laravel.php';
+require_once __DIR__ . '/deploy/assets.php';
+require_once __DIR__ . '/deploy/supervisor.php';
 
-// Set configurations
+// Specify the repository from which to download your project's code.
+// The server needs to have git installed for this to work.
+// If you're not using a forward agent, then the server has to be able to clone
+// your project from this repository.
 set('repository', 'https://github.com/j3j5/profe.dev.git');
-set('shared_files', ['.env']);
+
+// Define a server for deployment.
+server('production', '173.230.150.126', 213)
+    ->user('j3j5')
+    ->forwardAgent() // You can use identity key, ssh config, or username/password to auth on the server.
+    ->stage('production')
+    ->env('deploy_path', '/webservers/dep/profemariana.com'); // Define the base path to deploy your project to.
+
+
+server('beta', '173.230.150.126', 213)
+        ->user('j3j5')
+        ->forwardAgent() // You can use identity key, ssh config, or username/password to auth on the server.
+        ->stage('beta')
+        ->env('deploy_path', '/webservers/dep/beta.profemariana.com'); // Define the base path to deploy your project to.
+
+// Laravel shared dirs
 set('shared_dirs', [
     'storage/app',
     'storage/framework/cache',
@@ -16,47 +33,27 @@ set('shared_dirs', [
     'storage/framework/views',
     'storage/logs',
 ]);
-set('writable_dirs', ['bootstrap/cache', 'storage']);
-
-// Configure servers
-server('production', '173.230.150.126:213')
-    ->user('j3j5')
-    ->forwardAgent()
-    ->env('deploy_path', '/webservers/dep/profemariana.com');
-
-server('beta', '173.230.150.126:213')
-    ->user('j3j5')
-    ->forwardAgent()
-    ->env('deploy_path', '/webservers/dep/beta.profemariana.com');
-
 
 
 /**
- * TASKS
+ * Main task
  */
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'deploy:symlink',
+    'env:link',
+    'artisan:migrate',
+    'artisan:cache:clear',
+    'npm:install',
+    'assets:generate',
+    'log:set-permissions',
+    'supervisor:restart',
+    'cleanup',
+])->desc('Deploy your project');
 
-/**
- * Restart apache on success deploy.
- */
-task('apache:reload', function () {
-    run('sudo /usr/sbin/service apache2 reload');
-})->desc('Reload apache2 service');
-
-/**
- * Install NPM dependencies
- */
-task('npm:install', function () {
-    run('npm install');
-})->desc('Install NPM dependencies');
-
-/**
- * Compile assets with Gulp
- */
-task('gulp:compile', function () {
-    run('gulp --production');
-})->desc('Compile assets with gulp');
-
-after('deploy:vendors', 'npm:install');
-after('deploy:vendors', 'gulp:compile');
-before('success', 'artisan:migrate');
-after('success', 'apache:reload');
+after('deploy', 'success');
