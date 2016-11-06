a199f8e9330ae0c22ffdb5f90fb11340faf5f385
Julio Foulquie <jfoulquie@gmail.com>
2016-11-06 18:19:19 +0000

    Add acreditaciones view.

---
+++ resources/views/site/cursos/acreditaciones.blade.php
@@ -0,0 +1,15 @@
+@extends('site.layouts.master')
+
+@section('content')
+
+    @include('site.partials._cursos-header')
+
+
+    <div class="col-md-offset-3 col-md-6 well bg-info text-center">
+        <h2 class="">ACREDITACIÓN DE E.V.P. 1°</h2>
+	<h4>Se recomienda realizar los exámenes de 1° que figuran en esta página y luego presentarlos al docente del curso de 2°.</h4>
+	<h5>Más adelante se añadirán nuevos ejercicios en esta sección.</h5>
+    </div>
+
+@stop
+
