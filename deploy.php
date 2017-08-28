2e305e96e9b15c712912c7e0f9588d7eab850045
Julio Foulquie <jfoulquie@gmail.com>
2017-08-27 22:51:18 -0300

    Migrate deployer to v4.

--- deploy.php
+++ deploy.php
@@ -1,9 +1,14 @@
 <?php
 
-require_once 'recipe/common.php';
-require_once __DIR__ . '/deploy/laravel.php';
-require_once __DIR__ . '/deploy/assets.php';
-require_once __DIR__ . '/deploy/supervisor.php';
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// require_once __DIR__ . '/deploy/assets.php';
+// require_once __DIR__ . '/deploy/supervisor.php';
+
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
 
 // Specify the repository from which to download your project's code.
 // The server needs to have git installed for this to work.
@@ -16,45 +21,47 @@ server('production', '173.230.150.126', 213)
     ->user('j3j5')
     ->forwardAgent() // You can use identity key, ssh config, or username/password to auth on the server.
     ->stage('production')
-    ->env('deploy_path', '/webservers/profemariana.com'); // Define the base path to deploy your project to.
+    ->set('deploy_path', '/webservers/profemariana.com'); // Define the base path to deploy your project to.
 
 
 server('beta', '173.230.150.126', 213)
         ->user('j3j5')
         ->forwardAgent() // You can use identity key, ssh config, or username/password to auth on the server.
         ->stage('beta')
-        ->env('deploy_path', '/webservers/beta.profemariana.com'); // Define the base path to deploy your project to.
+        ->set('deploy_path', '/webservers/beta.profemariana.com'); // Define the base path to deploy your project to.
 
-// Laravel shared dirs
+set('writable_mode', 'chmod');
+set('shared_files', ['.env']);
 set('shared_dirs', [
-    'storage/app',
-    'storage/framework/cache',
-    'storage/framework/sessions',
-    'storage/framework/views',
-    'storage/logs',
+    'storage',
 ]);
+set('writable_dirs', ['bootstrap/cache', 'storage/app']);
 
 set('default_stage', 'production');
 
-/**
- * Main task
- */
-task('deploy', [
-    'deploy:prepare',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:vendors',
-    'deploy:writable',
-    'deploy:symlink',
-    'env:link',
-    'artisan:migrate',
-    'artisan:config:cache',
-    'artisan:route:cache',
-    'npm:install',
-    'assets:generate',
-    'log:set-permissions',
-    'cleanup',
-])->desc('Deploy your project');
+before('deploy:symlink', 'assets:generate');
+
+after('artisan:config:cache', 'artisan:route:cache');
+after('artisan:route:cache', 'artisan:migrate');
 
 after('deploy', 'success');
+
+set('bin/npm', function () {
+    return run('which npm')->toString();
+});
+
+task('npm:install', function () {
+    $npm_folder_exists = run('if [ ! -L {{deploy_path}}/shared/node_modules ] && [ -d {{deploy_path}}/shared/node_modules ]; then echo true; fi')->toBool();
+    if(!$npm_folder_exists) {
+        run('cd {{deploy_path}}/current; {{bin/npm}} install');
+        run('mv {{deploy_path}}/current/node_modules  {{deploy_path}}/shared');
+    }
+    run('ln -s {{deploy_path}}/shared/node_modules {{deploy_path}}/current/node_modules');
+})->desc('Execute npm install');
+
+task('assets:generate', function () {
+    run('cd {{deploy_path}}/current; gulp --production');
+})->desc('Generating assets');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
